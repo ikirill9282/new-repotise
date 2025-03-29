24638d92915f063109cc29e3421d1df5e9f2f3cc
xDaemonic <errewer123@gmail.com>
2025-03-30 00:02:30 +0300

    test email

---
+++ resources/views/emails/verify.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #f1f1f1;
+        }
+
+        .logo {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .logo img {
+            max-width: 200px;
+        }
+
+        .message {
+            padding: 20px;
+            background-color: #ffffff;
+        }
+
+        .message p {
+            margin-bottom: 10px;
+        }
+
+        .footer {
+            text-align: center;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="message">
+            <p>Dear {{ $mailData['name'] }},</p>
+            <p>Thank you for providing your details Your Company Name.
+        </div>
+    </div>
+</body>
+
+</html>
