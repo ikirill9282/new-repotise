214c906496f465af83535f488b5b2e3abc943cf5
xDaemonic <errewer123@gmail.com>
2025-06-08 16:35:12 +0300

    upload local

--- resources/views/site/header.blade.php
+++ resources/views/site/header.blade.php
@@ -49,6 +49,7 @@
                     </a>
                 @else
                     <a href="{{ auth()->user()->makeProfileUrl() }}" class="profile">
+                        @dd(auth()->user()->getAvatar())
                         <img src="{{ url(auth()->user()->getAvatar()) }}" alt="avatar"
                             class="profile_img">{{-- rounded-full w-8 --}}
                         <div class="right_text">
