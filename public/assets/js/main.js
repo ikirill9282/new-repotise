4c2effb1c38a8bbc12286861e0cab801edcb8d92
xDaemonic <errewer123@gmail.com>
2025-08-08 19:02:37 +0300

    upload local

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -399,7 +399,7 @@ function initModal() {
 function initCartSlider() {
     const heigth = window.innerHeight;
     const siblingHeight = $('.cart-order').outerHeight();
-    $('#cart-slider').css({ maxHeight: siblingHeight + 'px' })
+    $('#cart-slider').css({ height: siblingHeight + 'px !important' })
 
     let perView = 4.5;
 
