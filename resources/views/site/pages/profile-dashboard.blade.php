e69d423396635ab6778c672bf5568dc14d3ebf77
xDaemonic <errewer123@gmail.com>
2025-08-26 18:56:27 +0300

    upload

--- resources/views/site/pages/profile-dashboard.blade.php
+++ resources/views/site/pages/profile-dashboard.blade.php
@@ -76,39 +76,41 @@
                 </div>
             </div>
 
-            @livewire('profile.tables', [
-              'tables' => [
-                [
-                  'name' => 'sales',
-                  'title' => 'Sales Snapshot',
+            <x-card>
+              @livewire('profile.tables', [
+                'tables' => [
+                  [
+                    'name' => 'sales',
+                    'title' => 'Sales Snapshot',
+                  ],
+                  [
+                    'name' => 'product',
+                    'title' => 'Product Performance',
+                  ],
+                  [
+                    'name' => 'insights',
+                    'title' => 'Content Insights',
+                  ],
+                  [
+                    'name' => 'donation',
+                    'title' => 'Donation Summary',
+                  ],
+                  [
+                    'name' => 'refunds',
+                    'title' => 'Refunds Summary',
+                  ],
+                  [
+                    'name' => 'reviews',
+                    'title' => 'Recent Reviews',
+                  ],
+                  [
+                    'name' => 'referal',
+                    'title' => 'Referral Program Summary',
+                  ],
                 ],
-                [
-                  'name' => 'product',
-                  'title' => 'Product Performance',
-                ],
-                [
-                  'name' => 'insights',
-                  'title' => 'Content Insights',
-                ],
-                [
-                  'name' => 'donation',
-                  'title' => 'Donation Summary',
-                ],
-                [
-                  'name' => 'refunds',
-                  'title' => 'Refunds Summary',
-                ],
-                [
-                  'name' => 'reviews',
-                  'title' => 'Recent Reviews',
-                ],
-                [
-                  'name' => 'referal',
-                  'title' => 'Referral Program Summary',
-                ],
-              ],
-              'active' => 'sales',
-            ])
+                'active' => 'sales',
+              ])
+            </x-card>
         </div>
     </x-profile.wrap>
 @endsection
